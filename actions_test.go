9d64d3ff792b89019d1bf8d5d2d8b42cb4d2554c
Nelwhix <nelsonisioma1@gmail.com>
2022-08-16 12:24:56 +0100

    tool can list files in a dir

--- actions_test.go
+++ actions_test.go
@@ -23,6 +23,16 @@ func TestFilterOut(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			info, err := os.Stat(tc.file)
+			
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			f := filterOut(tc.file, tc.ext, tc.minSize, info)
+
+			if f != tc.expected {
+				t.Errorf("Expected '%t', got '%t' instead\n", tc.expected, f)
+			}
 		})
 	}
 }
